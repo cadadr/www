3930d498ba725ae978f02048626d73253db444c2
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-05-21 16:24:52 +0300

    A basic working thing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+
+import Data.Monoid (mappend)
+import System.FilePath.Posix (combine)
+import Hakyll
+import Hakyll.Core.Identifier (Identifier, fromFilePath)
+import Hakyll.Core.Identifier.Pattern (Pattern, fromGlob)
+
+
+contentDir :: FilePath
+contentDir = "content"
+
+templateDir :: FilePath
+templateDir = combine contentDir "templates"
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = hakyll $ do
+  match (pathTo "static/*") $ do
+    route $ delRoute "static/"
+    compile copyFileCompiler
+
+  match (pathTo "pages/*.markdown") $ do
+    route $ delRoute "pages/" `composeRoutes` setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate (tmpl "page.html") postCtx
+      >>= relativizeUrls
+
+  match (fromGlob $ combine templateDir "*") $ compile templateBodyCompiler
+
+
+delRoute :: String -> Routes
+delRoute r = gsubRoute (combine contentDir r) (const "")
+
+pathTo :: FilePath -> Pattern
+pathTo = fromGlob . combine contentDir
+
+tmpl :: FilePath -> Identifier
+tmpl = fromFilePath . combine templateDir
+
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
