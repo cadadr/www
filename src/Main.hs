72da2afdc2c1460125896eacc49876bac6a052e6
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-05-23 00:00:11 +0300

    Start the blog

--- src/Main.hs
+++ src/Main.hs
@@ -20,6 +20,14 @@ templateDir = combine contentDir "templates"
 
 main :: IO ()
 main = hakyll $ do
+  -- Pages
+  match (pathTo "pages/*.markdown") $ do
+    route $ delRouteAndSetExtension "pages/" "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate (tmpl "page.html") postCtx
+      >>= relativizeUrls
+
+  -- Static files
   match (pathTo "static/*") $ do
     route $ delRoute "static/"
     compile copyFileCompiler
@@ -32,17 +40,76 @@ main = hakyll $ do
     route $ delRoute "static/"
     compile copyFileCompiler
 
+  -- SCSS
   match (pathTo "scss/*.scss") $ do
-    route $ delRoute "scss/" `composeRoutes` setExtension "css"
+    route $ delRouteAndSetExtension "scss/" "css"
     compile sassCompiler
 
-  match (pathTo "pages/*.markdown") $ do
-    route $ delRoute "pages/" `composeRoutes` setExtension "html"
+  -- Personal blog.
+  let diyorkiBlogName = "Göktuğ diyor ki ..."
+  let diyorkiBlogUrl = "diyoki/index.html"
+
+  match ptn $ do
+    let diyorkiBlogCtx =
+          constField "blog" diyorkiBlogName `mappend`
+          constField "blogUrl" diyorkiBlogUrl `mappend`
+          postCtx
+    route $ delRouteAndSetExtension "blog-" "html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate (tmpl "page.html") postCtx
+      >>= loadAndApplyTemplate (tmpl "blog-diyoki/post.html") diyorkiBlogCtx
+      >>= saveSnapshot snap
+      >>= loadAndApplyTemplate (tmpl "page.html") diyorkiBlogCtx
       >>= relativizeUrls
 
-  match (fromGlob $ combine templateDir "*") $ compile templateBodyCompiler
+  -- TODO(2020-05-22): paginate
+  create [fromFilePath diyorkiBlogUrl] $ do
+    route $ idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAllSnapshots ptn snap
+      let archiveCtx =
+            listField "posts" postCtx (return posts) `mappend`
+            constField "blog" diyorkiBlogName `mappend`
+            constField "title" diyorkiBlogName `mappend`
+            constField "notitle" "yes" `mappend`
+            constField "blogUrl" diyorkiBlogUrl `mappend`
+            defaultContext
+      makeItem ""
+        >>= loadAndApplyTemplate (tmpl "blog-diyoki/listing.html") archiveCtx
+        >>= loadAndApplyTemplate (tmpl "page.html") archiveCtx
+        >>= relativizeUrls
+
+  create ["diyoki/diyoki.atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = postCtx `mappend` bodyField "description"
+      posts <- fmap (take 25) . recentFirst =<<
+        loadAllSnapshots ptn snap
+      renderAtom fc feedCtx posts
+
+  loadTemplatesAt Nothing
+  loadTemplatesAt $ Just "blog-diyoki"
+  where
+    ptn  = pathTo "blog-diyoki/*.markdown"
+    snap = "content-blog-diyoki"
+    fc :: FeedConfiguration
+    fc = FeedConfiguration
+      { feedTitle       = "Göktuğ diyor ki ..."
+      , feedDescription = "Göktuğ's rants and ramblings."
+      , feedAuthorName  = "Göktuğ Kayaalp"
+      , feedAuthorEmail = "self [at] gkayaalp [dot] com"
+      , feedRoot        = "https://www.gkayaalp.com/diyokí/"
+      }
+
+
+loadTemplatesAt :: Maybe FilePath -> Rules ()
+loadTemplatesAt (Just p) = loadTemplatesAt' (combine p "*")
+loadTemplatesAt Nothing  = loadTemplatesAt' "*"
+loadTemplatesAt' :: FilePath -> Rules ()
+loadTemplatesAt' p = match (fromGlob $ combine templateDir p) $ compile templateBodyCompiler
+
+
+delRouteAndSetExtension :: String -> String -> Routes
+delRouteAndSetExtension d x = delRoute d `composeRoutes` setExtension x
 
 
 delRoute :: String -> Routes
@@ -57,5 +124,5 @@ tmpl = fromFilePath . combine templateDir
 
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%FT%T%z" `mappend`
     defaultContext
